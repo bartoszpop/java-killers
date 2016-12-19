7ca2823bba9d8c3261d50f5787329e02faf7ac34
Bartosz Popiela <bartoszpop@gmail.com>
2016-12-19 23:15:42 +0100

    Example with a class visibility added.

---
+++ src/main/java/com/gmail/at/bartoszpop/javakillers/visibility/ClassVisibility.java
@@ -0,0 +1,10 @@
+package com.gmail.at.bartoszpop.javakillers.visibility;
+
+/*
+ * only public and package-private modifiers allowed for class declaration
+ */
+class SecondClass { // package-private visibility
+}
+
+public class ClassVisibility {
+}
