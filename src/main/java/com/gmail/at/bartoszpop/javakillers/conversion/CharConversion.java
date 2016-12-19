d29a0a53381ace57686ac170adf44101ea92c158
Bartosz Popiela <bartoszpop@gmail.com>
2016-12-19 23:10:10 +0100

    Example with a char conversion added.

---
+++ src/main/java/com/gmail/at/bartoszpop/javakillers/conversion/CharConversion.java
@@ -0,0 +1,9 @@
+package com.gmail.at.bartoszpop.javakillers.conversion;
+
+public class CharConversion {
+	public static void main(String[] args) {
+		char c = (char) 6;
+		System.out.println(c); // prints character with dec number 6
+		System.out.println(Character.getName(c)); // ACKNOWLEDGE
+	}
+}
