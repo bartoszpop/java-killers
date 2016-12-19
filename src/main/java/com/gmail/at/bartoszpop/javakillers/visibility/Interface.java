bcbd33be40610fe1e62dd0eb7d75df2d04be7893
Bartosz Popiela <bartoszpop@gmail.com>
2016-12-19 23:04:32 +0100

    Example with a visibility of interface members added.

---
+++ src/main/java/com/gmail/at/bartoszpop/javakillers/visibility/Interface.java
@@ -0,0 +1,7 @@
+package com.gmail.at.bartoszpop.javakillers.visibility;
+
+public interface Interface {
+	String field = "abc"; // public final by default
+	
+	void test(); // public abstract by default
+}
