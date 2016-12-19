9fd7938fbcad1ee6ed0a7e808b62230c80c64d55
Bartosz Popiela <bartoszpop@gmail.com>
2016-12-19 23:20:24 +0100

    Example with a boolean conversion added.

---
+++ src/main/java/com/gmail/at/bartoszpop/javakillers/conversion/BooleanConversion.java
@@ -0,0 +1,16 @@
+package com.gmail.at.bartoszpop.javakillers.conversion;
+
+public class BooleanConversion {
+	public static void main(String[] args) {
+		// boolean b;
+		// if (b = false) { // first assignment then evaluation
+		// System.out.println("true");
+		// } else {
+		// System.out.println("false"); // this line is printed
+		// }
+
+//		int i;
+//		if (i = 0) { // Compilation error "cannot convert from int to boolean"
+//		}
+	}
+}
