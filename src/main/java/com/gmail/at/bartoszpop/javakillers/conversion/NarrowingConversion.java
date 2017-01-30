2c8054673f27b6fa075dd184e6333dcc465af251
Bartosz Popiela <Bartosz.Popiela@smart4aviation.aero>
2017-01-30 13:57:31 +0100

    Example with a narrowing conversion added.

---
+++ src/main/java/com/gmail/at/bartoszpop/javakillers/conversion/NarrowingConversion.java
@@ -0,0 +1,9 @@
+package com.gmail.at.bartoszpop.javakillers.conversion;
+
+public class NarrowingConversion {
+    public static void main(String[] args) {
+        double d = 4.5;
+        int i = (int) d; //4
+        System.out.println(i);
+    }
+}
